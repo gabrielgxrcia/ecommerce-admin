c1d495c3920fd3ebb9b0229d0d6fd471029dc2c1
Gabriel <gabrielgrazeffi123@gmail.com>
2023-07-22 20:51:43 -0300

    data table (admin)

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -5,8 +5,15 @@ import { Button } from "@/components/ui/button"
 import { Heading } from "@/components/ui/heading"
 import { Separator } from "@/components/ui/separator"
 import { useParams, useRouter } from "next/navigation"
+import { BillboardColumn, columns } from "./columns"
+import { DataTable } from "@/components/ui/data-table"
+import { ApiList } from "@/components/ui/api-list"
 
-export const BillboardClient = () => {
+interface BilboardClientProps {
+  data: BillboardColumn[]
+}
+
+export const BillboardClient: React.FC<BilboardClientProps> = ({ data }) => {
   const router = useRouter()
   const params = useParams()
 
@@ -14,7 +21,7 @@ export const BillboardClient = () => {
     <>
       <div className="flex items-center justify-between">
         <Heading
-          title="Billboards (0)"
+          title={`Billboards (${data.length})`}
           description="Manage billboards for your store"
         />
         <Button
@@ -25,6 +32,10 @@ export const BillboardClient = () => {
         </Button>
       </div>
       <Separator />
+      <DataTable columns={columns} data={data} searchKey={"label"} />
+      <Heading title="API" description="API calls for Billboards" />
+      <Separator />
+      <ApiList entityName="billboards" entityIdName="billboardId" />
     </>
   )
 }
